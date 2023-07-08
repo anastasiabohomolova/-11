bcd1edc2e996ec26a4101c8a5fca4b129b69375c
Anastasia <136612325+anastasiabohomolova@users.noreply.github.com>
2023-07-08 22:10:44 +0300

    Update register.py

--- register.py
+++ register.py
@@ -27,20 +27,6 @@ def get_user_role(username_parol, users):
 
 
 
-#def register_user():
-    #users = {'admin': [], 'user': []}
-    #while True:
-        #username = input("Введіть ім'я користувача (або введіть 'q' для виходу): ")
-        #if username == 'q':
-            #break
 
-        #role = input("Виберіть роль (admin/user) для користувача '{}': ".format(username))
-        #if role not in ['admin', 'user']:
-            #print("Недійсна роль. Будь ласка, виберіть admin або user.")
-            #continue
 
-        #users[role].append(username)
-        #print("Користувач '{}'' успішно зареєстрований з роллю '{}'\n".format(username, role))
-
-    #return users
-
+        #
